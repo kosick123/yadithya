9a5dd1cc649f46c27ea526185fd9e86ca6dfb7ba
Kosickpaul <kousickpaulkareti@gmail.com>
2025-09-04 06:50:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Home from "./pages/Home";
+import Education from "./pages/Education";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Achievements from "./pages/Achievements";
+import Contact from "./pages/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/achievements" element={<Achievements />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
